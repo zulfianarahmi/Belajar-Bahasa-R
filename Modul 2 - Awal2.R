c8ecfde7857dd9cb9416ee38eec575a0cbd5eaab
Zulfiana Rahmi <103589769+zulfianarahmi@users.noreply.github.com>
2023-10-28 11:16:00 -1200

    Modul 2 part 2

---
+++ Modul 2 - Awal2.R
@@ -0,0 +1,19 @@
+ls(pat = "P")
+
+Nama <- "Zulfiana"
+NIM <- "0032"
+Nilai <- "A"
+
+#Menampilkan semua nama objek
+ls()
+
+ls(pat = "NIM")
+ls(pat = "^N")
+
+Nama <- "Zulfi"; NIM <- 0032; Nilai <- "A"
+
+#Infokan informasi objek secara detail
+ls.str(pat = "^N")
+
+# Menghapus objek?
+rm(Nama)
