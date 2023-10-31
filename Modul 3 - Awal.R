07c9f59ab15b349fc13b3c87d8241a5b24d84c21
Zulfiana Rahmi <103589769+zulfianarahmi@users.noreply.github.com>
2023-10-31 23:30:47 +0700

    Modul 3 Part 1

---
+++ Modul 3 - Awal.R
@@ -0,0 +1,32 @@
+#Objek dalam R punya 2 atribut intrinsik yaitu mode dan panjang
+#Mode adalah jenis / tipe data seuatu objek dan panjang adalah jumlah elemen dari suatu objek
+
+#Tipe data Numerik 
+n = 10.3
+class(n)
+
+x = 10
+class(x)
+
+#memeriksa apakah x ini integer
+is.integer(x)
+
+#bilangan yang sangat besar / kecil disebut eksponensial
+#17000
+n <- 1.7e7
+n
+
+#NA (not available) Inf (tak terhingga) NAN (NOt a number)
+x <- 50/0
+exp(x)
+exp(-x)
+x -x 
+
+x<- "Sabtu"
+x
+
+
+y <- "jum'at"
+cat(y)
+
+
